e0e54e1eaaceee44b95e71bce32c2336ffb3dfb4
q400400200-pixel <q.400400200@gmail.com>
2025-12-02 18:23:25 +0300

    Update 9-max_integer.py

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,5 +1,9 @@
-#!/usr/bin/python3
-def multiple_returns(sentence):
-    if len(sentence) == 0:
-        return (0, None)
-    return (len(sentence), sentence[0])
+ #!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    max_num = my_list[0]
+    for num in my_list:
+        if num > max_num:
+            max_num = num
+    return max_num
