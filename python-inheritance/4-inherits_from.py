28326f7442dae06f90733be197c8a98bcea7a074
q400400200-pixel <q.400400200@gmail.com>
2025-12-16 17:24:33 +0300

    Create 4-inherits_from.py

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module that defines inherits_from function"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Returns True if obj is an instance of a class that inherited from a_class
+
+    Args:
+        obj: The object to check
+        a_class: The class to compare with
+
+    Returns:
+        True if obj is an instance of a subclass of a_class
+        False if obj is exactly an instance of a_class or not related
+    """
+    return isinstance(obj, a_class) and type(obj) is not a_class
