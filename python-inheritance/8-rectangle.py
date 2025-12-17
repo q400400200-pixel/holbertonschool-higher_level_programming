033a520789be7639ef1a6546b9264e5b0b17117a
q400400200-pixel <q.400400200@gmail.com>
2025-12-18 02:09:27 +0300

    Create 8-rectangle.py

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module that defines Rectangle class"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class that inherits from BaseGeometry"""
+
+    def __init__(self, width, height):
+        """
+        Initialize a Rectangle instance
+
+        Args:
+            width: width of the rectangle (must be positive integer)
+            height: height of the rectangle (must be positive integer)
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
